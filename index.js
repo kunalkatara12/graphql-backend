cd16fee1297e8fa192ce93c8d2c66d8be612149f
kunalkatara12 <kunalkatara888@gmail.com>
2023-12-30 23:35:09 +0530

    graphql api

---
+++ index.js
@@ -0,0 +1,55 @@
+import app from "./app.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import axios from "axios";
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs: `
+    type User{
+        id:ID!
+        name:String!
+        username:String!
+        email:String!
+        phone:String!
+        website:String!
+
+    }
+        type Todo{
+            id:ID!
+            title:String!
+            completed:Boolean
+        }
+        type Query{
+            getTodos:[Todo]
+            getUsers:[User]
+            getUser(id:ID!):User
+        }
+        `,
+    resolvers: {
+      Query: {
+        getTodos: async () => {
+          return await axios
+            .get("https://jsonplaceholder.typicode.com/todos")
+            .then((res) => res.data);
+        },
+        getUsers: async () => {
+          return await axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.data);
+        },
+        getUser: async (parent, { id }) => {
+          return await axios
+            .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then((res) => res.data);
+        },
+      },
+    },
+  });
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(8000, () => {
+    console.log("Server started at port 8000");
+  });
+}
+startApolloServer();
